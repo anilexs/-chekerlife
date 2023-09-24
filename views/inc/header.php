19e60cc2cf9d3d8364bc58f2939ad9a0289a94c3
alexis <alexissimoes94@gmail.com>
2023-09-24 21:17:24 +0200

    v3

---
+++ views/inc/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://localhost/!chekerlife/asset/css/style.css">
+    <link rel="stylesheet" href="http://localhost/!chekerlife/asset/css/nav.css">
---
+++ views/inc/nav.php
@@ -0,0 +1,17 @@
+</head>
+<body>
+<?php if(1 != 1){ ?>
+
+    <?php }else{ ?>
+        <nav>
+            <form action="" method="get">
+                <input type="text" placeholder="Rechercher...">
+            </form>
+            <ul>
+                <li><a href="http://localhost/!chekerlife/">Accueil</a></li>
+                <li><a href="http://localhost/!chekerlife/categorie">Catégorie</a></li>
+                <li><a href="http://localhost/!chekerlife/connexion">connexion</a></li>
+                <li><a href="http://localhost/!chekerlife/inscription">inscription</a></li>
+            </ul>
+        </nav>
+    <?php } ?>
