5856f574c4381fd3c9928dcd8f56d34420b81bab
alexis <alexissimoes94@gmail.com>
2023-10-04 21:57:40 +0200

    v14 remise du foreach dant catalog a sup et continuet en js

--- views/categorie.php
+++ views/categorie.php
@@ -16,6 +16,26 @@ require_once "inc/header.php";
     <input type="text" placeholder="Rechercher..." id="rechercherCategorie">
 </div>
 <div class="catalog" id="catalog">
-    
-</div>
+    <?php foreach($catalog as $catalogItem){ ?>
+            <div class="card">
+                <?php
+                $isActive = false;
+                if(isset($_COOKIE['user_id'])){
+                    foreach($userLike as $like){
+                        if($like['catalog_id'] == $catalogItem["id_catalogue"] && $like['active'] == 1){
+                            $isActive = true;
+                            break;
+                        }
+                    }
+                }
+                ?>
+                <button class="like <?php echo $isActive ? 'activeTrue' : 'activeFalse'; ?>" id="<?= $catalogItem["id_catalogue"] ?>" onclick="like(<?= $catalogItem["id_catalogue"] ?>)">
+                    <i class="fa-solid fa-heart"></i>
+                </button>
+                <a href="list/<?= $catalogItem["nom"] ?>">
+                    <img src="asset/img/<?= $catalogItem["image_catalogue"] ?>" alt="">
+                </a>
+            </div>
+        <?php } ?>
+    </div>
 <?php require_once "inc/footer.php"; ?>
