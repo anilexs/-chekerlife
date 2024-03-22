f8b24d90eedc0ff880a58effdae073d31072741b
alexis <alexissimoes94@gmail.com>
2024-03-22 16:38:59 +0100

    v177

--- views/asset/js/parametre.js
+++ views/asset/js/parametre.js
@@ -11,7 +11,25 @@ $(document).on('click', '.navParametre', function(e) {
     var back = $('<div class="parametreBack"></div>');
         $('body').prepend(back);
     var left = $('<div class="parametreLeft"></div>');
+        left.append('<button id="parametreReturn"><i class="fa-solid fa-arrow-right fa-rotate-180"></i> retour</button>');
+
     var right = $('<div class="parametreRight"></div>');
         $('.parametreBack').append(left, right);
 
+
+    $(document).keydown(function(e) {
+        if (e.keyCode === 27) {
+            parametreReturn();
+        }
+    });
 });
+
+$(document).on('click', '#parametreReturn', function(e) {
+    parametreReturn();
+})
+
+function parametreReturn(){
+    $('.parametreBack').remove();
+    $('body').css('overflow', '');
+    $(document).off('keydown');
+}
